352ff610f4d316c1aa40029083c8bff2428d05c8
Ribesh <ribeshshr@gmail.com>
2025-12-06 15:23:25 +0000

    fix: added code for postgres

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import redis
+import psycopg2
 
 app = Flask(__name__)
 
@@ -12,6 +13,21 @@ def check_redis():
         return "Redis is UP!"
     except:
         return "Redis is DOWN!"
+
+@app.route("/db")
+def check_postgres():
+    try:
+        conn = psycopg2.connect(
+            host="postgres",
+            #database="ribesh",
+            user="postgres",
+            password="P@ssw0rd01"
+        )
+        conn.close()
+        return "Postgres is UP!"
+    except:
+        return "Posgres is DOWN!"
+        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
